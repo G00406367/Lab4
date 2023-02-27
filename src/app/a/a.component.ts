fb62fd538f1633e21bec259c6410b266fa766770
unknown <g00406367@atu.ie>
2023-02-27 10:22:37 +0000

    Lab4

---
+++ src/app/a/a.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-a',
+  templateUrl: './a.component.html',
+  styleUrls: ['./a.component.css']
+})
+export class AComponent {
+  name:string = 'Ben';
+  age:number = 20;
+
+}
